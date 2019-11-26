71a1758bf2383da746fba47cf6c53aec39caaa65
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-11-26 08:42:41 -0500

    adding constructor with fields, getters and setters, and toString method to ForumPost class

--- src/main/java/com/techtalentsouth/NeverAnonymous/ForumPost/ForumPost.java
+++ src/main/java/com/techtalentsouth/NeverAnonymous/ForumPost/ForumPost.java
@@ -18,6 +18,40 @@ public class ForumPost {
 	public ForumPost() {
 		
 	}
-	
+
+	public ForumPost(String author, String title, String forumEntry) {
+		this.author = author;
+		this.title = title;
+		this.forumEntry = forumEntry;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getForumEntry() {
+		return forumEntry;
+	}
+
+	public void setForumEntry(String forumEntry) {
+		this.forumEntry = forumEntry;
+	}
+
+	@Override
+	public String toString() {
+		return "ForumPost [id=" + id + ", author=" + author + ", title=" + title + ", forumEntry=" + forumEntry + "]";
+	}
 	
 }
