588dfeb1a18478962ff5e95290a84ecbb2df96f6
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-11-26 08:59:34 -0500

    adding a ForumPostRepository class and extending CrudRepository

---
+++ src/main/java/com/techtalentsouth/NeverAnonymous/ForumPost/ForumPostRepository.java
@@ -0,0 +1,7 @@
+package com.techtalentsouth.NeverAnonymous.ForumPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ForumPostRepository extends CrudRepository<ForumPost, Long> {
+
+}
