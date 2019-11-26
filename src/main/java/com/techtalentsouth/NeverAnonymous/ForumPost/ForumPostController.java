f53fb3ab7303c2a69d50afc6ceafd94f94a67935
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-11-26 08:51:48 -0500

    adding GetMapping annotation to index method in ForumPostController class

--- src/main/java/com/techtalentsouth/NeverAnonymous/ForumPost/ForumPostController.java
+++ src/main/java/com/techtalentsouth/NeverAnonymous/ForumPost/ForumPostController.java
@@ -1,10 +1,12 @@
 package com.techtalentsouth.NeverAnonymous.ForumPost;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ForumPostController {
 
+	@GetMapping(value="/")
 	public String index(ForumPost forumPost) {
 		return "forumpost/index";
 	}
