ea691a7e59d11e80c8b7f40a3990fbf4b08f3344
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-11-26 09:09:57 -0500

    adding ForumPostRepository and create method with PostMapping annotation to ForumPostController class

--- src/main/java/com/techtalentsouth/NeverAnonymous/ForumPost/ForumPostController.java
+++ src/main/java/com/techtalentsouth/NeverAnonymous/ForumPost/ForumPostController.java
@@ -1,13 +1,32 @@
 package com.techtalentsouth.NeverAnonymous.ForumPost;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ForumPostController {
+	
+	@Autowired
+	private ForumPostRepository forumPostRepository;
 
 	@GetMapping(value="/")
 	public String index(ForumPost forumPost) {
 		return "forumpost/index";
 	}
+	
+	private ForumPost forumPost;
+	
+	@PostMapping(value="/")
+	public String create(ForumPost forumPost, Model model) {
+		forumPostRepository.save(new ForumPost(forumPost.getTitle(), forumPost.getAuthor(), 
+		forumPost.getForumEntry()));
+		model.addAttribute("title", forumPost.getTitle());
+		model.addAttribute("author", forumPost.getAuthor());
+		model.addAttribute("forumEntry", forumPost.getForumEntry());
+		return "blogpost/result";
+	}
+	
 }
