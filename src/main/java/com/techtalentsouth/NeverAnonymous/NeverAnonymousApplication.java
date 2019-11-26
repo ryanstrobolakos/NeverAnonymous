1ee747269e601b565f97a6f3df3aef768240e75e
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-11-26 08:21:25 -0500

    initial commit

---
+++ src/main/java/com/techtalentsouth/NeverAnonymous/NeverAnonymousApplication.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.NeverAnonymous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverAnonymousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverAnonymousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtalentsouth/NeverAnonymous/NeverAnonymousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.NeverAnonymous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeverAnonymousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
